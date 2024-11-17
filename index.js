3ad90cef84494166c1ae508cea3ae1fbf51a1d64
Aditya Raj <adityaraj7320@gmail.com>
2024-11-17 16:26:38 +0530

    onscreen keyboard & textbox ui complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.addEventListener('keydown', (event) => {
     let input = event.key;
+
     const keys = document.querySelectorAll(".key");
+    //function for describing the onscreen keyboard behaviour.
     keys.forEach((key) => {
         let key_value =key.getAttribute("data-key");
         if (key_value===input || key_value.toLowerCase()===input) {
@@ -8,7 +10,10 @@ window.addEventListener('keydown', (event) => {
             setTimeout(() => {
                 key.classList.toggle("selected");
             }, 150);
-
         }
     })
+    //for taking the input and writing it on the input/output box
+    const output = document.querySelector("#output");
+
+
 })
