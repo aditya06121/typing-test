c75d558e9552dbefd6ab3f3a67f1c12a7863221f
Aditya Raj <adityaraj7320@gmail.com>
2024-11-20 13:15:46 +0530

    useless js removed

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-
-//global variable declarations
 let output = document.querySelector("#output");
 let count = 0;
 let content = "";
 let text_content;
 let index = 0;
-let words2, words1;
-let wpm;
-
-
+let modifiedContent = "";
 // Function to fetch and display text
 async function text_filler() {
   try {
@@ -80,99 +75,12 @@ function generateRandomNumber() {
 }
 const modifyText = function (index, type) {
   const content = output.textContent;
-  const modifiedContent =
+  modifiedContent =
     content.slice(0, index - 1) +
     `<span class="${type}">${content.charAt(index - 1)}</span>` +
     `<span class="grey">${content.charAt(index)}</span>` +
     content.slice(index + 1);
+  console.log(modifiedContent);
   output.innerHTML = modifiedContent;
 };
 
-//pop up
-//reload pop up
-//event listener
-const overlay = document.getElementById("overlay");
-const closeOverlayBtn = document.getElementById("closeOverlayBtn");
-const inpt_time = document.getElementById("time-limit");
-
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    overlay.classList.add("show"); // Add class to show the overlay
-  }, 500);
-  //display for mobiles
-  const x = window.outerWidth;
-  console.log(x);
-  if (x < 900) {
-    const display = document.querySelector("body");
-    display.outerHTML = `
-  <body style="background-color: black;color:white; hight:100% width:100%;position: fixed"><div style="font-size: 50px;padding:20px">Im sorry but this website is specially designed for desktop</div></body>
-  `;
-  }
-});
-
-// Close the overlay when the close button inside the overlay is clicked
-closeOverlayBtn.addEventListener("click", () => {
-  overlay.classList.remove("show");
-  let time = inpt_time.value;
-  setInterval(triggerEvent, time);
-});
-
-//results logic
-const finder = (index) => {
-  let x = text_content.slice(0, index);
-  words1 = x.trim().toLowerCase().split(/\s+/);
-  words2 = content.trim().toLowerCase().split(/\s+/);
-};
-const comparison = () => {
-  const wordCount1 = words1.length;
-  const wordCount2 = words2.length;
-
-  // Find common and different words
-  const commonWords = words1.filter((word) => words2.includes(word));
-  const differentWords = [
-    ...new Set([
-      ...words1.filter((word) => !words2.includes(word)),
-      ...words2.filter((word) => !words1.includes(word)),
-    ]),
-  ];
-
-  // Number of same and different words
-  const sameWordCount = commonWords.length;
-  const diffWordCount = differentWords.length;
-  wpm = sameWordCount * (60000 / inpt_time.value);
-  if (index === 0) {
-    return {
-      correctWords: 0, // Word count in the first string
-      typedWords: 0, // Word count in the second string
-      sameWordCount: 0, // Number of same words
-      diffWordCount: 0, // Number of different words
-      wpm: 0,
-    };
-  } else {
-    return {
-      correctWords: wordCount1, // Word count in the first string
-      typedWords: wordCount2, // Word count in the second string
-      sameWordCount: sameWordCount, // Number of same words
-      diffWordCount: diffWordCount, // Number of different words
-      wpm: wpm,
-    };
-  }
-};
-
-// Event handler function
-function triggerEvent() {
-  const overlay = document.getElementById("overlay");
-
-  finder(index);
-  const x = comparison();
-  const results = document.querySelector(".overlay-content");
-
-  results.innerHTML = `<p>Results</p>
-          <p>Number of words typed: ${x.typedWords}</p>
-          <p>Number of correct words typed: ${x.sameWordCount}</p>
-          <p style="font-size: 20px; font-weight: bold; color: #003579;">WPM: ${x.wpm}</p>
-          <p>Thanks for going over my hobby project</p>
-          <button onclick="location.reload()">Retake the test!</button>
-    `;
-  overlay.classList.add("show");
-}
